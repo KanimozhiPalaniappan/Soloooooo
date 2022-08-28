63bd07ffdb2d17ef7d35f1164a05383761c85728
KanimozhiPalaniappan <spkanimozhi004@gmail.com>
2022-08-28 22:25:53 +0530

    new one

---
+++ src/com/solo/Solos.java
@@ -0,0 +1,28 @@
+package com.solo;
+
+public class Solos {
+	
+
+	
+	         public static void main(String[] args) {
+					
+			        int a[]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			    int sum=0;
+
+			    for (int i = 0; i<=10; i++) {
+			    	sum += i;
+				}
+				System.out.println(sum);
+
+			    int n=a.length;
+
+				int average= sum/n;	
+
+					System.out.println(average);
+				}
+
+			}
+
+
+
+
