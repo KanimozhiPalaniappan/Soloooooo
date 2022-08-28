50262c33061da32ed6d54957b0966c0398f0e5af
KanimozhiPalaniappan <110536165+KanimozhiPalaniappan@users.noreply.github.com>
2022-08-28 22:42:49 +0530

    Update Solos.java

--- src/com/solo/Solos.java
+++ src/com/solo/Solos.java
@@ -6,7 +6,7 @@ public class Solos {
 	
 	         public static void main(String[] args) {
 					
-			        int a[]= {1, 2, 3, 4, 5, 6};
+			        int a[]= {1, 2, 3, 4, 5};
 			    int sum=0;
 
 			    for (int i = 0; i<=10; i++) {
